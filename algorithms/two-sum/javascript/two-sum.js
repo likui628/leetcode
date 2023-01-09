ec39dd64665c315036dd797d6279b93e4fc46800
Li Kui <90845831+likui628@users.noreply.github.com>
2023-01-09 21:14:14 +0800

    update(javascript,cpp): 1. Two Sum

--- algorithms/two-sum/javascript/two-sum.js
+++ algorithms/two-sum/javascript/two-sum.js
@@ -1,3 +1,4 @@
+// solution 1
 /**
  * @param {number[]} nums
  * @param {number} target
@@ -9,6 +10,27 @@ var twoSum = function (nums, target) {
       if (nums[i] + nums[j] === target) {
         return [i, j];
       }
+    }
+  }
+};
+
+// solution 2
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function (nums, target) {
+  let map = new Map();
+  nums.forEach((value, index) => {
+    map.set(value, index);
+  });
+
+  for (let i = 0; i < nums.length; i++) {
+    let complement = target - nums[i];
+
+    if (map.has(complement) && i !== map.get(complement)) {
+      return [map.get(complement), i];
     }
   }
 };
